91df60e494de9abd5c26edcdc87e13a3b0c4f58c
Michael D. Hoyle <hoylemd@gmail.com>
2015-03-02 12:47:06 -0500

    whitespace changes

--- serializers.py
+++ serializers.py
@@ -1,11 +1,17 @@
-from django.forms import widgets
 from django.contrib.auth.models import User
 
 from rest_framework import serializers
+from charman.models import Character
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-  # example owned stuff link: snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail')
+    # example owned stuff link: snippets =
+    #  serializers.HyperlinkedRelatedField(many=True,
+    #                                      view_name='snippet-detail')
+    characters = serializers.HyperlinkedRelatedField(many=True,
+            view_name='character-detail',
+            queryset=Character.objects.all())
 
-  class Meta:
-    model = User
-    fields = ('url', 'username')
+    class Meta:
+        model = User
+        fields = ('url', 'username')
